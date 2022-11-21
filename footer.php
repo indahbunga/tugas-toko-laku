eeb2b05b8fb4b5093258d0621d8ba607ed04bce7
indahbunga <118707107+indahbunga@users.noreply.github.com>
2022-11-21 11:05:25 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" href="style.css">
+<script src="fontawesome/js/all.min.js"></script>
+<footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>Tentang Kami</h2>
+          <div class="content">
+            <p>CodinNepal is a YouTube channel where you can learn web designing, web development, ui/ux designing, html css tutorial, hover animation and effects, javascript and jquery tutorial and related so on.</p>
+            <div class="social">
+              <a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
+              <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="center box">
+          <h2>Alamat</h2>
+          <div class="content">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">Babat, Lamongan</span>
+            </div>
+            <div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+089-765432100</span>
+            </div>
+            <div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">djazulial32@gmail.com</span>
+            </div>
+          </div>
+        </div>
+        <div class="right box">
+          <h2>Kontak Kami</h2>
+          <div class="content">
+            <form action="#">
+              <div class="email">
+                <div class="text">Email *</div>
+                <input type="email" required>
+              </div>
+              <div class="msg">
+                <div class="text">Pesan *</div>
+                <textarea id=".msgForm" rows="2" cols="25" required></textarea><br>
+
+              <div class="btn">
+                <button class="btn btn-success" type="submit">Kirim</button>
+              </div>
+            </form>
+          </div>
+          <div class="bottom">
+	        <center>
+	          <span class="credit">Created By <a href="https://youtube.com/ppnurulummah">Djazuli Ahmad</a> </span>
+	          <span class="far fa-copyright"></span> 2020 All rights reserved.
+	        </center>
+	      </div>
+	      
+     
